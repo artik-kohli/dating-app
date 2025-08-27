7b4228c11a90d280ca27c91850360235b3cb1e97
artik-kohli <stillwaterartius@gmail.com>
2025-08-28 00:29:54 +0530

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -8,8 +8,13 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 var app = builder.Build();
+
+// Middleware
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
+    .WithOrigins("http://localhost:4200", "https://localhost:4200"));
 
 app.MapControllers();
 
